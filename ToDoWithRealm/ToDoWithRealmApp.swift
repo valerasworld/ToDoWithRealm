a5ff76ed17c8d412e610b895cad50f7fc8ccb1f9
Valery Zazulin <zazulinvalerii@gmail.com>
2024-03-27 19:56:15 +0100

    Initial Commit

---
+++ ToDoWithRealm/ToDoWithRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoWithRealmApp.swift
+//  ToDoWithRealm
+//
+//  Created by Валерий Зазулин on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoWithRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
